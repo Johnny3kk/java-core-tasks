b5c1bad56ecc29c94a9f659826a030b3bc0a2fa2
Johnny3kk <some@mail.ru>
2022-02-25 00:50:04 +0300

    Main update;

--- src/Main.java
+++ src/Main.java
@@ -12,18 +12,36 @@ public class Main {
     Marshmallow mar = new Marshmallow("marsh", 330, new BigDecimal("88"), 50);
     HardCandy hc = new HardCandy("candy", 10, new BigDecimal("10"), 77);
     Cupcake cup = new Cupcake("cake", 450, new BigDecimal("110"), 44);
+    Chocolate cho1 = new Chocolate("alenka", 200, new BigDecimal("79"), 35);
+    HardCandy hc1 = new HardCandy("big_chups", 90, new BigDecimal("150"), 88);
     Box box = new BoxImpl();
-
+    Box box1 = new BoxImpl();
     box.add(cho);
     box.add(mar);
     box.add(hc);
     box.add(cup);
+    box.add(cho1);
+    box.add(hc1);
+    box1.add(cho);
+    box1.add(mar);
+    box1.add(hc);
+    box1.add(cup);
+    box1.add(cho1);
+    box1.add(hc1);
+
+    System.out.println(box.getInfo());
+    System.out.println(box.getBoxWeight());
+    minWeightBoxOptimization(900, box);
     System.out.println(box.getInfo());
     System.out.println(box.getBoxWeight());
 
-    minPriceBoxOptimization(800, box);
-    System.out.println(box.getInfo());
-    System.out.println(box.getBoxWeight());
+    System.out.println("=============================================================");
+
+    System.out.println(box1.getInfo());
+    System.out.println(box1.getBoxWeight());
+    minPriceBoxOptimization(900, box1);
+    System.out.println(box1.getInfo());
+    System.out.println(box1.getBoxWeight());
   }
 
   public static void minWeightBoxOptimization(int weightBorder, Box box) {
@@ -45,6 +63,7 @@ public class Main {
       }
     }
   }
+
   public static void minPriceBoxOptimization(int weightBorder, Box box) {
     int steps = box.getSize();
     if (box.getBoxWeight() > weightBorder) {
