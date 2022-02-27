e192bb4288c8411cac53d5b3e2092d68e6e41f20
Johnny3kk <some@mail.ru>
2022-02-27 15:09:39 +0300

    getting data procedure;

--- src/java2/Main.java
+++ src/java2/Main.java
@@ -1,8 +1,32 @@
 package java2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException {
 
+    Scanner sc = new Scanner(new File("src/java2/texts/most_important_text.txt"));
+    List<String> list = new ArrayList<>();
+    while (sc.hasNext()) {
+      list.addAll(
+          Arrays.asList(
+              sc.nextLine()
+                  .trim()
+                  .replaceAll("'", " ")
+                  .toLowerCase()
+                  .replaceAll("[^a-z]", " ")
+                  .split("\\s+")));
+      if (list.get(list.size() - 1).equals("")) {
+        list.remove(list.size() - 1);
+      }
+    }
+
+    System.out.println(list);
   }
 }
