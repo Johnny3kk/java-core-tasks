1b9c694a6c3e56a13ebf65fa3c8a5db0b20025f3
Johnny3kk <some@mail.ru>
2022-02-27 20:40:02 +0300

    almost finish;

--- src/java2/Main.java
+++ src/java2/Main.java
@@ -2,14 +2,22 @@ package java2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
 
-  public static void main(String[] args) throws FileNotFoundException {
+  public static void main(String[] args) throws Exception {
+    // пример пути "src\\java2\\texts\\most_important_text.txt"
+    // пример пути D:\Yandex\most_important_text.txt
 
-    Scanner sc = new Scanner(new File("src/java2/texts/most_important_text.txt"));
+    System.out.println(
+        "Введите абсолютный или относительный (проекта) путь для исследумого текстового файла:");
     List<String> list = new ArrayList<>();
+try{
+    Scanner console = new Scanner(System.in);
+    String in = console.nextLine();
+    Scanner sc = new Scanner(new File(in));
     while (sc.hasNext()) {
       list.addAll(
           Arrays.asList(
@@ -23,14 +31,17 @@ public class Main {
         list.remove(list.size() - 1);
       }
     }
+} catch (Exception e) {
+      System.out.println("Пожалуйста внимательнее вводите путь к исследуемому файлу");
+}
+
 
     System.out.println(list);
     percentUsingWordsPrintList(list);
     maxUsingWord(list);
   }
 
-  public static void percentUsingWordsPrintList(List<String> list) {
-    int wordSum = list.size();
+  public static Map<String, Integer> inputDataRatingMapCreation(List<String> list) {
     Map<String, Integer> map = new HashMap<>();
 
     for (int i = 0; i < list.size(); i++) {
@@ -38,6 +49,12 @@ public class Main {
         map.put(list.get(i), map.get(list.get(i)) + 1);
       } else map.put(list.get(i), 1);
     }
+    return map;
+  }
+
+  public static void percentUsingWordsPrintList(List<String> list) {
+    int wordSum = list.size();
+    Map<String, Integer> map = inputDataRatingMapCreation(list);
 
     Map<String, Integer> sortedMap = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
     sortedMap.putAll(map);
@@ -52,13 +69,7 @@ public class Main {
     int wordSum = list.size();
     int maxValue = Integer.MIN_VALUE;
     List<String> maxResults = new ArrayList<>();
-    Map<String, Integer> map = new HashMap<>();
-
-    for (int i = 0; i < list.size(); i++) {
-      if (map.containsKey(list.get(i))) {
-        map.put(list.get(i), map.get(list.get(i)) + 1);
-      } else map.put(list.get(i), 1);
-    }
+    Map<String, Integer> map = inputDataRatingMapCreation(list);
 
     for (Map.Entry<String, Integer> m : map.entrySet()) {
       if (m.getValue() == maxValue) {
