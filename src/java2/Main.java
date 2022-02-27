01b53f98d429ee3b520153e72c49a87f174e20ce
Johnny3kk <some@mail.ru>
2022-02-27 16:04:37 +0300

    add percentUsingWordsPrintList() method;

--- src/java2/Main.java
+++ src/java2/Main.java
@@ -2,10 +2,7 @@ package java2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -17,10 +14,10 @@ public class Main {
       list.addAll(
           Arrays.asList(
               sc.nextLine()
-                  .trim()
                   .replaceAll("'", " ")
                   .toLowerCase()
                   .replaceAll("[^a-z]", " ")
+                  .trim()
                   .split("\\s+")));
       if (list.get(list.size() - 1).equals("")) {
         list.remove(list.size() - 1);
@@ -28,5 +25,22 @@ public class Main {
     }
 
     System.out.println(list);
+    percentUsingWordsPrintList(list);
+  }
+
+  public static void percentUsingWordsPrintList(List<String> list) {
+    int wordSum = list.size();
+    Map<String, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < list.size(); i++) {
+      if (map.containsKey(list.get(i))) {
+        map.put(list.get(i), map.get(list.get(i)) + 1);
+      } else map.put(list.get(i), 1);
+    }
+
+    for (Map.Entry<String, Integer> m : map.entrySet()) {
+      double value = (double) m.getValue() * 100 / wordSum;
+      System.out.println(m.getKey() + ": " + String.format("%.2f", value) + "%");
+    }
   }
 }
