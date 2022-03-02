4847cb115786098f70cce941a6edf71d171b318e
Johnny3kk <some@mail.ru>
2022-03-02 16:15:04 +0300

    Main updated with method companiesFoundedAfterDate();

--- src/java3/Main.java
+++ src/java3/Main.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -39,26 +40,53 @@ public class Main {
     expiredSecurities(companies.getCompanies());
     System.out.println("=============================================================================================================================================");
     System.out.println("1. Для получения информации о компаниях, основанных позже определённой даты, введите запрос в виде:" +
-            " ДД.ММ.ГГ; ДД.ММ,ГГ; ДД/ММ/ГГГГ; ДД/ММ/ГГ.\n" +
+            " ДД.ММ.ГГГГ; ДД.ММ,ГГ; ДД/ММ/ГГГГ; ДД/ММ/ГГ.\n" +
             "2. Для вывода информации о ценных бумагах, использующих определённую валюту, введите запрос в виде:" +
             " EU; USD; RUB");
     String in = console.nextLine();
-    System.out.println(in);
+    if (Arrays.stream(in.split("")).anyMatch(s -> s.matches("\\d"))) {
+        companiesFoundedAfterDate(in, companies.getCompanies());
+    } else  securitiesByCurrency(in, companies.getCompanies());
   }
 
   public static void expiredSecurities(List<Company> list) {
     list.stream()
         .forEach(
             c -> {
-              System.out.print(c.getName() + ": ");
+              System.out.print(c.getName() + " ценные бумаги с истёкшим сроком: ");
               c.getSecurities().stream()
                   .filter(s -> LocalDate.parse(s.getDate(), formatter1).isBefore(TODAY))
                   .forEach(
                       s ->
                           System.out.print(
-                              "{" + s.getCode() + ", " + s.getDate() + ", " + s.getName() + "}"));
-              System.out.println(" количество истёкших ценных бумаг компании = " + c.getSecurities().stream()
+                              "{" + s.getCode() + ", " + s.getDate() + ", " + s.getName() + "}, "));
+              System.out.println("количество истёкших ценных бумаг компании = " + c.getSecurities().stream()
                       .filter(s -> LocalDate.parse(s.getDate(), formatter1).isBefore(TODAY)).count());
             });
   }
+
+  public static void companiesFoundedAfterDate(String str, List<Company> list) {
+    List<String> strings = Arrays.asList(str.split(""));
+    DateTimeFormatter formatter;
+    if (strings.size() > 8) {
+        if (strings.stream().anyMatch(s -> s.matches("/"))) {
+            formatter = formatter3;
+        } else formatter = formatter1;
+    } else {
+        if (strings.stream().anyMatch(s -> s.matches("/"))) {
+            formatter = formatter4;
+        } else formatter = formatter2;
+    }
+    list.stream().filter(c -> LocalDate.parse(c.getFounded(), formatter1).isAfter(LocalDate.parse(str, formatter)))
+            .forEach(c ->
+                    System.out.println(
+                            c.getName()
+                                    + " - Дата основания "
+                                    + LocalDate.parse(c.getFounded(), formatter1).format(formatter4)));
+  }
+
+  public static void securitiesByCurrency(String str, List<Company> list) {
+    System.out.println("Вижу буквы");
+  }
+
 }
